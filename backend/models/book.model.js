e5412dafe94fefdadfc9a7a9d782f1c319847ec0
chwello <fretchelm.gerarman@gmail.com>
2024-12-21 18:45:14 +0530

    upate schema

--- backend/models/book.model.js
+++ backend/models/book.model.js
@@ -1,31 +1,54 @@
 import mongoose from "mongoose";
 
 const bookSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		author: {
-			type: String,
-			required: true,
-		},
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
     description: {
-			type: String,
-			required: true,
-		},
+      type: String,
+      required: true,
+    },
     genre: {
-			type: String,
-			required: true,
-		},
-		image: {
-			type: String,
-			required: true,
-		},
-	},
-	{
-		timestamps: true, // createdAt, updatedAt
-	}
+      type: String,
+      enum: [
+        'fiction',
+        'non-fiction',
+        'mystery',
+        'fantasy',
+        'romance',
+        'science-fiction',
+        'biography',
+        'historical',
+        'self-help',
+        'thriller',
+        'horror',
+        'poetry',
+      ],
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['to-be-read', 'reading', 'finished', 'dropped', 're-reading'],
+      required: true,
+    },
+    reviews: {
+      type: String, 
+      default: '',
+    },
+  },
+  {
+    timestamps: true, 
+  }
 );
 
 const Book = mongoose.model("Book", bookSchema);
