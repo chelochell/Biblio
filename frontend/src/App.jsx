765c4b1a7f124f8d31069744e4081419d1c88abc
chwello <fretchelm.gerarman@gmail.com>
2024-12-21 19:18:36 +0800

    create route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import {Button} from '@chakra-ui/react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CreateBook from './pages/CreateBook';
+import HomePage from './pages/HomePage';
 
 function App() {
-  
-
   return (
-    <>
-    <Button>hiii</Button>
-    </>
-  )
+    <div className='p-6'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AddBook" element={<CreateBook />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-
 import React from 'react'; 
-import ReactDOM from 'react-dom/client'; 
+import { createRoot } from 'react-dom/client'; 
 import App from './App.jsx'; 
-import { ChakraProvider } from '@chakra-ui/react'; 
+import './index.css';
+import {BrowserRouter} from 'react-router-dom'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider> 
-      <App /> 
-    </ChakraProvider>
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode> 
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
