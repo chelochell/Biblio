92a01e610dad88f71b4990468247c005d7a5bcc1
chelojing <fretchelgerarman461@gmail.com>
2025-01-09 22:37:13 +0800

    setup auth

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -22,6 +22,16 @@ const UserSchema = new mongoose.Schema({
         ref: "Post",
       },
     ],
+    lastLogin:{
+      type:Date,
+      default:Date.now,
+      isVerified:false
+    },
+    resetPasswordToken:String,
+    resetPasswordExpiresAt:Date,
+    verificationToken:String,
+    verificationTokenExpiresAt:Date,
+
   }, { timestamps: true });
 
   const User = mongoose.model("User", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import bookRouters from "./routes/bookroutes.js";
 import uploadRoutes from "./routes/uploadroutes.js";
 import clubRouter from "./routes/clubroutes.js"
+import authRoutes from "./routes/auth.routes.js"
 import multer from "multer";
 import path from 'path';
 import cors from 'cors';
@@ -77,6 +78,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
 app.use("/api/books", bookRouters);
 app.use("/api/uploads", uploadRoutes);
 app.use("/api/clubs", clubRouter);
+app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:" + PORT);
